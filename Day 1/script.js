1bfa5368bb9ef6491fb833a14afda47999e85042
Tuan1601 <NATTuan1601@gmail.com>
2024-09-11 08:44:18 +0700

    Tuan1601

---
+++ Day 1/script.js
@@ -0,0 +1,7 @@
+var the = document.getElementById("the");
+var moi = document.createElement("moi");
+moi.className = "Demo";
+moi.innerHTML = "Nguyễn Anh Tuấn"
+moi.id = "heading"
+moi.textContent='Nguyễn Anh Tuấn'
+the.appendChild(moi);
