874bb1b6b4b8bc3664e98f6aedb5e6eee3bcf8ba
Tuan1601 <NATTuan1601@gmail.com>
2024-10-30 16:03:17 +0700

    Tuan1601

--- TH_30_10_2024/api_mysql/index.js
+++ TH_30_10_2024/api_mysql/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const todosRouter = require('./src/routers/todos');
+const todos1Router = require('./src/routers/todos1');
 
 app.use(express.json());
 
 app.use('/todos', todosRouter);
+app.use('/todos1', todos1Router);
 
-app.listen(3001, () => {
+app.listen(3002, () => {
     console.log('Server is running on port 3001');
 });
